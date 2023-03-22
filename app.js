ee70b52cdf00cc837e45f44cef3dfccac8e2c3c9
unknown <ogabr2020@gmail.com>
2023-03-21 22:38:50 -0500

    new commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.locals.abook = 'abook';
+app.locals.abook = 'aboook';
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
